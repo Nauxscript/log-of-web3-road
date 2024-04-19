245434ad2d6982ac8dd8f33f0ab4bc6a141ce9cc
nauxscript <nauxscript@gmail.com>
2024-04-19 21:49:53 +0800

    wip: mint token

--- Mantle_Learning_Track/The_First_Contract/main.sol
+++ Mantle_Learning_Track/The_First_Contract/main.sol
@@ -7,4 +7,16 @@ contract MyToken {
   constructor(){
       owner = msg.sender;
   }
+
+  function mint(address recipient, uint256 amount) public {
+    // Only the owner can mint tokens
+    require(msg.sender == owner, "You are not the owner");
+
+    // Increase the balance of the recipient by the amount
+    balances[recipient] += amount;
+
+    // Increase the total supply, because new tokens are created
+    totalSupply += amount;
+
+  }
 }
