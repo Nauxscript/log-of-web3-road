f80996e1c2ae516a6bfe4fd374ca60533dea4edc
nauxscript <nauxscript@gmail.com>
2024-04-23 21:44:02 +0800

    wip: state variables

--- Mantle_Learning_Track/CryptoKitties/main.sol
+++ Mantle_Learning_Track/CryptoKitties/main.sol
@@ -4,4 +4,15 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SimpleCryptoKitties is ERC721 {
   uint256 public _tokenIdCounter = 1;
+
+  struct Kitty {
+    uint256 genes;
+    uint256 birthTime;
+    uint256 momId;
+    uint256 dadId;
+    uint256 generation;
+  } 
+
+  mapping (uint256 => Kitty) public kitties;
+
 }
