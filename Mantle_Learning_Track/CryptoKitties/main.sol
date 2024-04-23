aaf18d3896771161412722d5d2bc58982af0bad0
nauxscript <nauxscript@gmail.com>
2024-04-23 21:44:30 +0800

    feat: createKitty

--- Mantle_Learning_Track/CryptoKitties/main.sol
+++ Mantle_Learning_Track/CryptoKitties/main.sol
@@ -17,4 +17,10 @@ contract SimpleCryptoKitties is ERC721 {
 
   constructor() ERC721("SimpleCryptoKitties", "SCK") {}
 
+  function _createKitty(uint256 momId, uint256 dadId, uint256 generation, uint256 genes, address owner) private returns(uint256) {
+
+    kitties[_tokenIdCounter] = Kitty(genes, block.timestamp, momId, dadId, generation);
+    _mint(owner, _tokenIdCounter);
+    return _tokenIdCounter++;
+  }
 }
