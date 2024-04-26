4f820f77ba79ae01f865a9570a775dc406564584
nauxscript <nauxscript@gmail.com>
2024-04-26 21:37:11 +0800

    wip: still has error in calling chainlink VRF

--- No_Lie_Lottery_Bot/main.sol
+++ No_Lie_Lottery_Bot/main.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+
 struct ProbabilityItem {
   string name;
   uint256 p;
@@ -14,12 +16,39 @@ struct Rule {
   bool available;
 }
 
-contract Lottery {
+contract Lottery is VRFConsumerBase {
+
   uint256 nextRuleId = 1;
   mapping (uint256 => Rule) ruleset;
   mapping (address => uint256[]) owner2rules;
 
-  constructor() {}
+  bytes32 internal keyHash;
+  uint256 internal fee;
+  uint256 private randomResult;
+
+  mapping(bytes32 => uint256) requestToRuleId;
+
+  constructor() 
+        VRFConsumerBase(
+            0xf720CF1B963e0e7bE9F58fd471EFa67e7bF00cfb, // VRF Coordinator
+            0x20fE562d797A42Dcb3399062AE9546cd06f63280  // LINK Token
+        )
+    {
+        keyHash = 0xced103054e349b8dfb51352f0f8fa9b5d20dde3d06f9f43cb2b85bc64b238205;
+        fee = 0.1 * 10 ** 18; // 0.1 LINK
+    }
+
+    /**
+     * Callback function used by VRF Coordinator
+     */
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        randomResult = randomness % 100;
+        // Use the random number to select a reward
+        Rule storage rule = ruleset[requestToRuleId[requestId]];
+        string memory reward = this.select(rule.lotteryProbabilities);
+        // Do something with the reward, e.g., emit an event
+        emit RewardSelected(requestId, reward);
+    }
 
   function createRule(string[] memory rewardNames, uint256[] memory rewardProbabilitis) public returns(uint256) {
     require(rewardNames.length == rewardProbabilitis.length);
@@ -54,9 +83,33 @@ contract Lottery {
     return ruleset[ruleId];
   }
 
+  function select(ProbabilityItem[] memory probabilities) view external returns (string memory) {
+        
+        uint sum = 0;
+        for(uint i = 0; i < probabilities.length; i++) {
+            sum += probabilities[i].p;
+            if(randomResult < sum) {
+                return probabilities[i].name;
+            }
+        }
+
+        return '';
+    }
+
+    function getReward(uint256 ruleId) public returns(bytes32) {
+        // Rule storage rule = ruleset[ruleId];
+        // return this.select(rule.lotteryProbabilities);
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
+        bytes32 requestId = requestRandomness(keyHash, fee);
+        requestToRuleId[requestId] = ruleId;
+        return requestId;
+    }
+
   fallback() external {
     revert("something wrong");
   }
+
+  event RewardSelected(bytes32 requestId, string reward);
 }
 
 // ["Phone", "Laptop", "Mouse", "Nothing, Damn!"]
